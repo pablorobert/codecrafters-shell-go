6610318f5d7f0b403a2929fa9930565da55e30f6
Pablo Alves <pablorobert@gmail.com>
2024-05-27 20:48:33 -0300

    REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,12 +8,14 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-	// Wait for user input
-	str, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		panic(err)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		// Wait for user input
+		str, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		str = strings.Replace(str, "\n", "", -1)
+		fmt.Printf("%s: command not found\n", str)
 	}
-	str = strings.Replace(str, "\n", "", -1)
-	fmt.Printf("%s: command not found\n", str)
 }
